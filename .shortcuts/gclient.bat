cac9595dee2d9f331318d01488b214a3be2aeb04
laurentjiang <laurent.jiang@epitech.eu>
2024-01-04 15:55:01 +0100

    (port) fix: the whole build, running is still not functional

---
+++ .shortcuts/gclient.bat
@@ -0,0 +1,16 @@
+@REM equivalent to this shell script but for windows
+@REM #!/bin/bash
+
+@REM ./build/ecs/run_engine --config-dir=game_rtype/.engine/config --server-port=8080 --server-ip=127.0.0.1
+
+@REM set the path to the engine
+set engine_path=build\ecs\run_engine.exe
+
+@REM set the path to the config directory
+set config_dir=game_rtype/.engine/config
+
+@REM set the port to use
+set port=8080
+
+@REM run the engine
+%engine_path% --config-dir=%config_dir% --server-port=%port% --server_ip=127.0.0.1
